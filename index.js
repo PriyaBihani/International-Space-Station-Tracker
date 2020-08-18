6f6a4169aa8af2d5b1c2aa4a5e4c2e64444f5e38
Priya-Bihani <priyabihani123@gmail.com>
2020-08-18 12:58:07 +0530

    Internation space station tracker

---
+++ index.js
@@ -0,0 +1,41 @@
+ //First it is going to make a map with a blank grey box in the line 2 then next line onwards it will start making tiles only when it is connected to the leaflet.js library through internet 
+ // TO avoid the default marker icon we are gonna create a default marker icon in line 12th onwards. Then we are gonna set initial marker lat lon and icon and add to map and store it in a variable  
+ const mymap = L.map('issMap').setView([0, 0], 1);
+ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoia2FydGlrMThnIiwiYSI6ImNrMmg0ZXByMTAyYnEzY29laHIxYmJ6NzIifQ.jNjY-tnIhfG-GTMxc2jDbw'
+}).addTo(mymap);
+
+//Making a marker with a icon
+var issIcon = L.icon({
+    iconUrl: 'iss200.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+});
+
+const marker =  L.marker([0, 0],{icon: issIcon}).addTo(mymap);// just telling the marker where it is going to be placed in the map
+ 
+//ISS is obviously a fake api but great for learning. Here we grab the data from this api and set the lat lon on the map
+// Fetch will fetch the data from the url and convert it to readable format and set the view and display lat lon
+// Respective function has been set to control the zoom level of map when it loads for the first time and others 
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+let firstTime= true
+
+async function getData(){
+
+   const response = await fetch(api_url)// fetching data from an api
+   const data = await response.json()// converting into a readable format
+   const {latitude, longitude} = data 
+   if (firstTime){
+   mymap.setView([latitude,longitude], 2)
+     firstTime = false
+   }
+   marker.setLatLng([latitude,longitude])
+   document.getElementById('lat').textContent= latitude.toFixed(2)
+   document.getElementById('lon').textContent= longitude.toFixed(2)
+ 
+}
+ getData()
+ setInterval(getData,2000)
